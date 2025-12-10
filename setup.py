d1139a51340b4138c47d30cd46fb1ac86b31b0a0
D3-4D <bernardouss0ps@gmail.com>
2025-12-10 03:19:42 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="enem-extractor",
+    version="1.0.0",
+    description="Ferramenta para baixar provas e gabaritos do ENEM diretamente do INEP.",
+    author="D3-4D",
+    license="Apache-2.0",
+    packages=find_packages(),
+    py_modules=["downloader"],
+    install_requires=[
+        "requests",
+        "beautifulsoup4"
+    ],
+    extras_require={
+        "progress": ["tqdm"],
+        "colors": ["colorama"],
+        "all": ["tqdm", "colorama"],
+    },
+    entry_points={
+        "console_scripts": [
+            "downloader-enem=downloader:Download",
+        ]
+    },
+    python_requires=">=3.10",
+)
